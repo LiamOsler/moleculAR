fb722b1470138003ad2fc22eaf4c7b96524e5b5e
Liam Osler <osler.liam@gmail.com>
2023-02-13 19:42:18 -0400

    update item display and routes

--- routes/item.js
+++ routes/item.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs'); 
 
+let data = JSON.parse(fs.readFileSync('./public/catalog/catalog.json', "utf8"));
+
+console.log(data)
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/catalog');
@@ -9,12 +13,23 @@ router.get('/', function(req, res, next) {
 
 router.get('/:id', function(req , res){
   var molfiles = fs.readdirSync('./public/molfiles/')
+
   for(let i = 0; i < molfiles.length; i++){
     molfiles[i] = molfiles[i].replace('.mol', '');
   }
   if(molfiles.includes(req.params.id)){
+    if(!data[req.params.id].hasOwnProperty('name')){
+      data[req.params.id].name = 'No name in catalog';
+      data[req.params.id].formula = 'No formula in catalog';
+    }
     var molfile = fs.readFileSync('./public/molfiles/'+req.params.id+'.mol', 'utf8');
-    res.render('item', {title: 'Molecule Viewer: '+ req.params.id, item: req.params.id, molfile: molfile});
+    res.render('item', {
+      title: 'Molecule Viewer: '+ req.params.id, 
+      item: req.params.id, 
+      molfile: molfile,
+      name: data[req.params.id].name,
+      formula: data[req.params.id].formula,
+    });
   }else{
     res.render('error', {title: 'Error', message: 'Molecule not found', error: {status: 404, stack: ''}});
   }
