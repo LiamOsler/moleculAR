16553166a43bd266631cca81dec45c10f106a0e6
Liam Osler <osler.liam@gmail.com>
2023-02-16 21:40:55 -0400

    update app

--- routes/catalog.js
+++ routes/catalog.js
@@ -5,8 +5,6 @@ var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     const molfiles = './public/molfiles/';
-    const catalog = './public/catalog/catalog.json';
-
     res.render('catalog', { title: 'Catalog', list: fs.readdirSync(molfiles)});
 });
 
